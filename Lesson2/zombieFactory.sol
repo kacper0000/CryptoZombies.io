e1569844706386245b9b819171ebc0966dbefd9e
kacper0000 <57183004+kacper0000@users.noreply.github.com>
2021-09-08 20:14:03 +0100

    Update and rename 2_zombieFactory.sol to zombieFactory.sol

--- Lesson2/zombieFactory.sol
+++ Lesson2/zombieFactory.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.5.0 <0.6.0;
+  
+pragma solidity ^0.4.25;
 
 contract ZombieFactory {
 
@@ -17,19 +18,21 @@ contract ZombieFactory {
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string memory _name, uint _dna) private {
+    function _createZombie(string _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(string memory _str) private view returns (uint) {
+    function _generateRandomDna(string _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
 
-    function createRandomZombie(string memory _name) public {
+    function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
-
-}
