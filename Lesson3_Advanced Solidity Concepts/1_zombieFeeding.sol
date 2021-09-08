d19ab2e70436ffc0b749170ff1e7e3caeb8deb34
kacper0000 <57183004+kacper0000@users.noreply.github.com>
2021-09-08 20:16:56 +0100

    Update 1_zombieFeeding.sol

--- Lesson3_Advanced Solidity Concepts/1_zombieFeeding.sol
+++ Lesson3_Advanced Solidity Concepts/1_zombieFeeding.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.5.0 <0.6.0;
+pragma solidity ^0.4.25;
 
 import "./zombiefactory.sol";
 
@@ -21,11 +21,11 @@ contract ZombieFeeding is ZombieFactory {
 
   KittyInterface kittyContract;
 
-  function setKittyContractAddress(address _address) external {
+  function setKittyContractAddress(address _address) external onlyOwner {
     kittyContract = KittyInterface(_address);
   }
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
